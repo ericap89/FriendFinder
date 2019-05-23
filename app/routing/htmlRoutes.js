c13f634e5f2b942b566610d0fd02cbf181b991b9
Erica P <erica.parra@rocketmail.com>
2019-05-23 12:26:40 -0700

    route to home workng not to survey

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,1 +1,11 @@
 // Build 'path' 
+
+const { join } = require('path')
+
+module.exports = app => {
+    app.get('/', (req, res) => {
+        //use direct path to home page  
+        res.sendFile(join(__dirname, '../public/home.html'))
+    })
+    
+}
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-// server connection 
+// Bring in pkg requires
 const express = require('express');
 const { join } = require('path');
 const app = express();
 
-app.use(express.static(join(__dirname, 'public')))
+// Middleware
+app.use(express.static(join(__dirname, './public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+// Routes 
+const routes = require('./app/routing/htmlRoutes')(app)
+
+// Listen port
 app.listen(3000)
