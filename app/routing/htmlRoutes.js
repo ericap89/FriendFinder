d72bdcf313f477291b063e6e0fa4588a7cadddc6
Erica P <erica.parra@rocketmail.com>
2019-05-28 17:13:09 -0700

    Paths for html fixed

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,17 @@
-// Build 'path' 
+//Build paths to static html 
 
-const { join } = require('path')
+var path = require('path');
 
-module.exports = app => {
-    app.get('/', (req, res) => {
-        //use direct path to home page  
-        res.sendFile(join(__dirname, '../public/home.html'))
-    })
-    
-}
+
+module.exports = function(app)
+{ //path to home 
+    app.get('/', function (req,res)
+    {
+      res.sendFile(path.join(__dirname , '../public/home.html'));
+    });
+  //path to survey
+    app.get('/survey.html', function (req, res) 
+    {
+      res.sendFile(path.join(__dirname , '../public/survey.html'));
+    });
+} 
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 // Bring in pkg requires
 const express = require('express');
-const { join } = require('path');
+
+// var path = require("path");
 const app = express();
 
 // Middleware
-app.use(express.static(join(__dirname, './public')))
+app.use(express.static(__dirname + "public"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 // Routes 
-const routes = require('./app/routing/htmlRoutes')(app)
+require('./app/routing/htmlRoutes.js')(app);
 
-// Listen port
-app.listen(3000)
+
+
+// Listen port search env first 
+const PORT = process.env.PORT || 3000
+// console log response in terminal 
+app.listen(PORT, () => console.log(`Server loaded on port: ${PORT}`))
